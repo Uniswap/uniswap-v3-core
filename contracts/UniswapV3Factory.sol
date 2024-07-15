70d2223319f8c2eed0dcd52a3ed4a7842874db4b
Kevin Weaver <kevinweaver2@gmail.com>
2024-07-15 14:33:47 -0700

    Add additional fees and tick spacing

--- contracts/UniswapV3Factory.sol
+++ contracts/UniswapV3Factory.sol
@@ -23,12 +23,42 @@ contract UniswapV3Factory is IUniswapV3Factory, UniswapV3PoolDeployer, NoDelegat
         owner = _owner;
         emit OwnerChanged(address(0), _owner);
 
-        feeAmountTickSpacing[500] = 10;
+        feeAmountTickSpacing[500] = 10; // 0.05%
         emit FeeAmountEnabled(500, 10);
-        feeAmountTickSpacing[3000] = 60;
+        feeAmountTickSpacing[3000] = 60; // 0.3%
         emit FeeAmountEnabled(3000, 60);
-        feeAmountTickSpacing[10000] = 200;
+        feeAmountTickSpacing[5000] = 100; // 0.5%
+        emit FeeAmountEnabled(5000, 100);
+        feeAmountTickSpacing[10000] = 200; // 1%
         emit FeeAmountEnabled(10000, 200);
+        feeAmountTickSpacing[20000] = 400; // 2%
+        emit FeeAmountEnabled(20000, 400);
+        feeAmountTickSpacing[30000] = 600; // 3%
+        emit FeeAmountEnabled(30000, 600);
+        feeAmountTickSpacing[40000] = 800; // 4%
+        emit FeeAmountEnabled(40000, 800);
+        feeAmountTickSpacing[50000] = 1000; // 5%
+        emit FeeAmountEnabled(50000, 1000);
+        feeAmountTickSpacing[60000] = 1200; // 6%
+        emit FeeAmountEnabled(60000, 1200);
+        feeAmountTickSpacing[70000] = 1400; // 7%
+        emit FeeAmountEnabled(70000, 1400);
+        feeAmountTickSpacing[80000] = 1600; // 8%
+        emit FeeAmountEnabled(80000, 1600);
+        feeAmountTickSpacing[90000] = 1800; // 9%
+        emit FeeAmountEnabled(90000, 1800);
+        feeAmountTickSpacing[100000] = 2000; // 10%
+        emit FeeAmountEnabled(100000, 2000);
+        feeAmountTickSpacing[110000] = 2200; // 11%
+        emit FeeAmountEnabled(110000, 2200);
+        feeAmountTickSpacing[120000] = 2400; // 12%
+        emit FeeAmountEnabled(120000, 2400);
+        feeAmountTickSpacing[130000] = 2600; // 13%
+        emit FeeAmountEnabled(130000, 2600);
+        feeAmountTickSpacing[140000] = 2800; // 14%
+        emit FeeAmountEnabled(140000, 2800);
+        feeAmountTickSpacing[150000] = 3000; // 15%
+        emit FeeAmountEnabled(150000, 3000);
     }
 
     /// @inheritdoc IUniswapV3Factory
